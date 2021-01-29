052d0ae94d99aa08b813acdaa5ce4961c1c8c317
jessicagomesng <jgnpeluso@gmail.com>
2021-01-29 15:26:00 +0000

    adding validations to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password 
+
     has_many :story_users
     has_many :stories, :through => :story_users
+
+    validates :username, :password, :presence => true
+    validates :password, :length => { minimum: 7 }
+    validates :password, :confirmation => true, unless -> { password.blank? }
 end
